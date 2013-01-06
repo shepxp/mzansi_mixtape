4bf259330f6688d5f2a09de812a9d7cb255d8267
Shepherd Nkomo <shepxp@gmail.com>
2013-01-06 23:32:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MzansiMixtape::Application.config.secret_token = '8e8583e89c1b8a011dee83d2742eb0a10400be9ab47851170235b4d5d5c50d29246d9625f44dffa2696ddfa31e4c28542760851a7521d04e6617b9bc5c47d9e6'
